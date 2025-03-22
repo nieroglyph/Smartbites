d830566517ffbc873af3a1338338e54d733703dd
Rein Mark <reinmark.delrosario@gmail.com>
2025-03-22 19:57:34 +0800

    create user profiles model

--- smartbites/api/models.py
+++ smartbites/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User  # Default Django User model
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dietary_preference = models.CharField(max_length=100, choices=[
+        ('vegan', 'Vegan'),
+        ('keto', 'Keto'),
+        ('vegetarian', 'Vegetarian'),
+        ('omnivore', 'Omnivore'),
+    ], default='omnivore')
+    allergies = models.TextField(blank=True, null=True)
+    caloric_needs = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
