f884c48547fcf31c813c9f3aa3d4a94143d2a1f6
Rein Mark <reinmark.delrosario@gmail.com>
2025-03-22 19:34:19 +0800

    test api, create mysql database

--- smartbites/smartbites/settings.py
+++ smartbites/smartbites/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'smartbites.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'smartbites',
+        'USER': 'root',
+        'PASSWORD': 'hyeju1113',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
