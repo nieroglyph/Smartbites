36e170236e33988b9daeeb70840462e68da3ad39
Rein Mark <reinmark.delrosario@gmail.com>
2025-03-22 21:42:43 +0800

    implement authentication tokens

--- smartbites/api/urls.py
+++ smartbites/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 # User Registration, Login, Logout
-from .views import register_user, login_user, logout_user
+from .views import register_user, login_user, logout_user, my_secure_view
 
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('authentication/', my_secure_view, name='authentication')
 ]
--- smartbites/api/views.py
+++ smartbites/api/views.py
@@ -46,3 +46,15 @@ def logout_user(request):
     request.user.auth_token.delete()
     logout(request)
     return Response({'message': 'Logged out successfully'}, status=status.HTTP_200_OK)
+
+# User Authentication
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+@api_view(["GET"])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def my_secure_view(request):
+    return Response({"message": "Welcome, authorized user!"})
