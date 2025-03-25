84b7c2f46d8ac03ce9155dc0d93c6f4875f34575
Rein Mark <reinmark.delrosario@gmail.com>
2025-03-25 19:32:27 +0800

    allow auto-assign user profile

--- smartbites/api/signals.py
+++ smartbites/api/signals.py
@@ -1,15 +1,17 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from .models import UserProfile
 
-@receiver(post_save, sender=User)
+CustomUser = get_user_model()  # Get custom user model
+
+@receiver(post_save, sender=CustomUser)
 def create_user_profile(sender, instance, created, **kwargs):
     """Create a user profile when a new user registers"""
     if created:
-        UserProfile.objects.create(user=instance)
+        UserProfile.objects.create(user=instance, dietary_preference='omnivore')  # Set default
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=CustomUser)
 def save_user_profile(sender, instance, **kwargs):
     """Save user profile when user updates account"""
     instance.userprofile.save()
